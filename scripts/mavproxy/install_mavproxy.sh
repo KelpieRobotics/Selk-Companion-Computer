d527026c4609c6ecfbaa6953dd1e310a0c9d8a27
ymoumin <ymoum044@uottawa.ca>
2024-10-19 18:54:15 -0400

    BIG BAD path override

--- scripts/mavproxy/install_mavproxy.sh
+++ scripts/mavproxy/install_mavproxy.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame
 pip3 install PyYAML mavproxy --user
-echo 'export PATH="/home/ubuntu/.local/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
 
 # copy service to correct directory
 sudo cp mavproxy.service /etc/systemd/system/
