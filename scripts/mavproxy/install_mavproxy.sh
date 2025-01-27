807e847d63662845851b0bcac606130c1d56105a
Fufs <42976807+Fufs@users.noreply.github.com>
2025-01-27 11:06:58 -0500

    PATCHED: update mavproxy scripts + add gstreamer bash scripts
    Co-authored-by: ymoumin <ymoum044@uottawa.ca>

--- scripts/mavproxy/install_mavproxy.sh
+++ scripts/mavproxy/install_mavproxy.sh
@@ -1,8 +1,12 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
 sudo apt install -y python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame
 pip3 install PyYAML mavproxy --user
-echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc # TODO: Don't append if it already exists
 
 # copy service to correct directory
 sudo cp mavproxy.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl enable mavproxy
+sudo systemctl start mavproxy
--- scripts/mavproxy/start_mavproxy.sh
+++ scripts/mavproxy/start_mavproxy.sh
@@ -1,7 +1,3 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
-sudo systemctl daemon-reload
-sudo systemctl enable mavproxy
-sudo systemctl daemon-reload
-sudo service mavproxy start
-systemctl status mavproxy.service
+mavproxy.py --master=/dev/ttyAMA0 --baudrate=115200 --out=udp:0.0.0.0:14550 --aircraft=Sub --cmd="set flushlogs True" --daemon
