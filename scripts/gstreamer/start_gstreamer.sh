81b4001530cfa0d5f5a9b4a1f5b999be1c001614
Chess-Dude <68159447+Chess-Dude@users.noreply.github.com>
2025-02-27 15:00:39 -0500

    - updated connection paths for pi and devices - documented with view what each camera/pixhawk should go into - note that you cannot use by-id for cameras (as they have the same id) - pixhawk uses by-id, camera uses by-path

--- scripts/gstreamer/start_gstreamer.sh
+++ scripts/gstreamer/start_gstreamer.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
-gst-launch-1.0 -v v4l2src device=/dev/video3 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5600 sync=false
-gst-launch-1.0 -v v4l2src device=/dev/video1 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5601 sync=false
-gst-launch-1.0 -v v4l2src device=/dev/video2 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5602 sync=false
+# Cam 1
+gst-launch-1.0 -v v4l2src platform-xhci-hcd.0-usbv2-0:0:1:1.0-video-index2 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5600 sync=false
+
+# Cam 2
+gst-launch-1.0 -v v4l2src platform-xhci-hcd.0-usbv2-0:2:1.0-video-index2 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5601 sync=false
+
+# Cam 3
+gst-launch-1.0 -v v4l2src platform-xhci-hcd.1-usbv2-0:2:1.0-video-index2 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5602 sync=false
+
+
+# USB PORTS as they are plugged in
+
+#   USB0         USB2
+# top left       top right
+# Pixhawk        Cam 1
+
+#   USB1         USB3
+# bottom left     bottom right
+#  Cam 2          Cam 3
--- scripts/mavproxy/start_mavproxy.sh
+++ scripts/mavproxy/start_mavproxy.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/bash
 
-mavproxy.py --master=/dev/ttyAMA0 --baudrate=115200 --out=udp:0.0.0.0:14550 --aircraft=Sub --cmd="set flushlogs True" --daemon
+mavproxy.py usb-ArduPilot_Pixhawk4-bdshot_290042000251333035363236-if02 --baudrate=115200 --out=udp:0.0.0.0:14550 --aircraft=Sub --cmd="set flushlogs True" --daemon
+
+# POSSIBLE OTHER PATH (UNSURE)
+# usb-ArduPilot_Pixhawk4-bdshot_290042000251333035363236-if00
+
+# USB PORTS as they are plugged in
+
+#   USB0         USB2
+# top left       top right
+# Pixhawk        Cam 1
+
+#   USB1         USB3
+# bottom left     bottom right
+#  Cam 2          Cam 3
