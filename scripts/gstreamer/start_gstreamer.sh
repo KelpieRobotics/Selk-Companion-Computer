b5ba46d397986bd01bfc53df00b46ea75f489b00
Fufs <42976807+Fufs@users.noreply.github.com>
2025-05-29 18:48:43 -0400

    fix and simplify gstreamer script

--- scripts/gstreamer/start_gstreamer.sh
+++ scripts/gstreamer/start_gstreamer.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/bash
 
-parallel --line-buffer << END
-gst-launch-1.0 -v \
-    v4l2src device=/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._exploreHD_USB_Camera_DWE-0001-video-index2 ! \
+parallel -j 3 --line-buffer --retry-failed \
+    gst-launch-1.0 -v \
+    v4l2src device=/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._exploreHD_USB_Camera_DWE-000{}-video-index2 ! \
     video/x-h264, width=1920,height=1080 ! \
     h264parse ! \
     queue ! \
     rtph264pay config-interval=10 pt=96 ! \
-    udpsink host=192.168.137.255 port=5600 sync=false
-
-gst-launch-1.0 -v \
-    v4l2src device=/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._exploreHD_USB_Camera_DWE-0002-video-index2 ! \
-    video/x-h264, width=1920,height=1080 ! \
-    h264parse ! \
-    queue ! \
-    rtph264pay config-interval=10 pt=96 ! \
-    udpsink host=192.168.137.255 port=5601 sync=false
-
-gst-launch-1.0 -v \
-    v4l2src device=/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._exploreHD_USB_Camera_DWE-0003-video-index2 ! \
-    video/x-h264, width=1920,height=1080 ! \
-    h264parse ! \
-    queue ! \
-    rtph264pay config-interval=10 pt=96 ! \
-    udpsink host=192.168.137.255 port=5602 sync=false
-END
+    udpsink host=192.168.137.255 port=560{} sync=false \
+    ::: 1 2 3
