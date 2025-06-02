cf32bb654367462f1ed677363e57bb62822fdda3
Fufs <42976807+Fufs@users.noreply.github.com>
2025-06-02 17:13:53 -0400

    oopsie woopsie made a typo OwO

--- scripts/mavproxy/start_mavproxy.sh
+++ scripts/mavproxy/start_mavproxy.sh
@@ -3,6 +3,6 @@
 mavproxy.py --daemon \
     --master=/dev/ttyS6 \
     --baudrate=115200 \
-    --out=udpbcast:192.168.137.255:14450 \
+    --out=udpbcast:192.168.137.255:14550 \
     --aircraft=Sub \
     --cmd="set flushlogs True"
