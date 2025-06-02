e00e351fcffa5c07d1ac865b29f7bc78d3a85e74
Fufs <42976807+Fufs@users.noreply.github.com>
2025-06-02 16:42:50 -0400

    fix mavproxy start script

--- scripts/mavproxy/start_mavproxy.sh
+++ scripts/mavproxy/start_mavproxy.sh
@@ -3,6 +3,6 @@
 mavproxy.py --daemon \
     --master=/dev/ttyS6 \
     --baudrate=115200 \
-    --out=udp:0.0.0.0:14550 \
+    --out=udpbcast:192.168.137.255:14450 \
     --aircraft=Sub \
-    --cmd="set flushlogs True" \
+    --cmd="set flushlogs True"
