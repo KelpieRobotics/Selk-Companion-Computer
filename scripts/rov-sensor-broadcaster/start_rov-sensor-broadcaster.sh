2061e0edadf28db9397e28bf6a59fd105286535a
Fufs <42976807+Fufs@users.noreply.github.com>
2025-06-17 17:40:37 -0400

    rename rov-temperature-broadcaster to rov-sensor-logger in scripts

--- scripts/rov-sensor-broadcaster/start_rov-sensor-broadcaster.sh
+++ scripts/rov-sensor-broadcaster/start_rov-sensor-broadcaster.sh
@@ -2,4 +2,4 @@
 
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
-$REPO_DIR/modules/rov-temperature-broadcaster/build/logger
+$REPO_DIR/modules/rov-sensor-broadcaster/build/logger
