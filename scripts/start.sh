4824ce82a52c8fd188f654229c7827f42aba4a44
Fufs <42976807+Fufs@users.noreply.github.com>
2025-06-17 17:36:26 -0400

    ensure parallel launches all 4 processes

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-parallel --line-buffer --retry-failed <<END
+parallel -j 4 --line-buffer --retry-failed <<END
 bash $REPO_DIR/scripts/mavproxy/start_mavproxy.sh
 bash $REPO_DIR/scripts/gstreamer/start_gstreamer.sh
 bash $REPO_DIR/scripts/selk_rc_receiver/start_selk_rc_receiver.sh
